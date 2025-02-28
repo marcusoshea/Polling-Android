13b35f8c76a1ba84e69edb4074b84a28e2077328
Marcus O'Shea <marcusoshea100@gmail.com>
2025-02-27 21:38:42 -0500

    Set reminder for release

--- app/src/main/java/com/pollingandroidv2/util/Constants.kt
+++ app/src/main/java/com/pollingandroidv2/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.pollingandroidv2.util
 
 object Constants {
+    //TODO Replace with your own Key_Alias before releasing
     const val KEY_ALIAS = "MyKeyAlias"
     const val ANDROID_KEY_STORE = "AndroidKeyStore"
 }
